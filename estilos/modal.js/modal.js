0701ed200b52de1602f76dfbf54e9fe34f52c797
docente2022 <107597018+docente2022@users.noreply.github.com>
2023-03-22 17:59:05 -0300

    cambios realizados correctamentes. falta dar estylo y arreglar responsive

---
+++ estilos/modal.js/modal.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    /*Almacenar como variable la URL (valor del atributo src del elemento iframe) del video*/
+    var url = $("#four-seasons-video").attr('src');
+    
+    /*Valor vacío para el atributo src de iframe cuando se cierra la ventana modal, con lo cual se detiene la reproducción del video*/  
+    $("#myModal").on('hide.bs.modal', function(){
+          $("#four-seasons-video").attr('src', '');
+      });
+    
+    /*Volver a asignar la url almaceneda*/
+    $("#myModal").on('show.bs.modal', function(){
+          $("#four-seasons-video").attr('src', url);
+      });
+  });
